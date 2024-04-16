9cd5b737efa52ca29602006a6b21331df4a2b310
ShadowSlayer03 <sslayer441@gmail.com>
2024-04-16 13:38:53 +0530

    Added Login and SignUp frontend code

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react/dist";
 /*
   This example requires some changes to your config:
   
@@ -13,6 +16,108 @@
   ```
 */
 export default function Homepage() {
+  const [email, setEmail] = useState();
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+  const [signup, setSignup] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const handleLogIn = async () => {
+    if (!email || !password) {
+      toast({
+        title: "Please Fill All the Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `/auth/login`,
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userToken", JSON.stringify(data));
+      navigate("/notes");
+    } catch (error) {
+      console.log("Error Occurred!", error);
+      toast({
+        title: "Login Unsuccessful!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+
+  const handleSignUp = async () => {
+    setLoading(true);
+    if (!name || !email || !password) {
+      toast({
+        title: "Please Fill All the Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `/auth/signUp/`,
+        { name, email, password },
+        config
+      );
+
+      toast({
+        title: "Registration Successful!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userToken", JSON.stringify(data));
+      navigate("/notes");
+    } catch (error) {
+      console.log("Error Occurred!", error);
+      toast({
+        title: "Registration Unsuccessful!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+
   return (
     <>
       <div className="h-[100vh] flex items-center justify-center ">
@@ -23,13 +128,38 @@ export default function Homepage() {
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
+            {signup ? (
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign Up
+              </h2>
+            ) : (
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Log In
+              </h2>
+            )}
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
+              {signup && (
+                <div className="mt-2">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="name"
+                    autoComplete="current-password"
+                    onChange={() => setName(name)}
+                    required
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              )}
               <div>
                 <label
                   htmlFor="email"
@@ -44,7 +174,8 @@ export default function Homepage() {
                     type="email"
                     autoComplete="email"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={() => setEmail(email)}
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -73,21 +204,57 @@ export default function Homepage() {
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={() => setPassword(password)}
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
+                {signup ? (
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      handleSignUp;
+                    }}
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign Up
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      handleLogIn;
+                    }}
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Log In
+                  </button>
+                )}
+                {signup ? (
+                  <div className="flex mt-5 gap-2">
+                    <p>Already a User?</p>
+                    <a
+                      className="underline text-blue-500 cursor-pointer"
+                      onClick={() => setSignup(false)}
+                    >
+                      Log In
+                    </a>
+                  </div>
+                ) : (
+                  <div className="flex mt-5 gap-2">
+                    <p>New User?</p>
+                    <a
+                      className="underline text-blue-500 cursor-pointer"
+                      onClick={() => setSignup(true)}
+                    >
+                      Sign up Here
+                    </a>
+                  </div>
+                )}
               </div>
             </form>
-
           </div>
         </div>
       </div>
