7006ecb172b2bfab61b4decab21613e440722e8f
Antriksh Narang <antrikshnarang6@gmail.com>
2024-04-16 12:01:41 +0530

    Created Middleware

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+
+const JWT_SECRET_STRING = process.env.JWT_SECRET_STRING;
+
+const fetchuser = (req, res, next) => {
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).json({error: 'Authenticate using a valid token'});
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET_STRING);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).json({error: 'Authenticate using a valid token'});
+    }
+}
+
+module.exports = fetchuser;
